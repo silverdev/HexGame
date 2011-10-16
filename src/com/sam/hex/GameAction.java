67b855c9cad9b314e234a6b9f7bdac7a65b30e1b
Nicholas Jeffrey <njeffre2@illinois.edu>
2011-10-15 20:36:12 -0500

    Changed background to be correct

--- src/com/sam/hex/GameAction.java
+++ src/com/sam/hex/GameAction.java
@@ -10,7 +10,7 @@ public class GameAction {
 	public static void updateBoard() {
 		double radius;
 		RegularPolygonGameObject[][] gamePeace = Global.gamePeace;
-		 radius =BoardTools.radiusCalculator(HexGameWindow.cPolygons.getWidth(),HexGameWindow.cPolygons.getHeight(), 7);
+		 radius =BoardTools.radiusCalculator(HexGameWindow.cPolygons.getWidth(),HexGameWindow.cPolygons.getHeight(), Global.gridSize);
 	//	radius = BoardTools.radiusCalculator(400,400, 7);
 		double hrad = radius * Math.sqrt(3) / 2; // Horizontal radius
 		int yOffset=(int)((HexGameWindow.cPolygons.getHeight()-((3*radius/2)*(gamePeace[0].length-1)+2*radius))/2);
@@ -18,9 +18,7 @@ public class GameAction {
 		
 		for (int xc = 0; xc < Global.gamePeace.length; xc++) {
 			for (int yc = 0; yc < gamePeace[0].length; yc++)
-				gamePeace[xc][yc].update( (int)(hrad + yc * hrad + 2
-						* hrad * xc)+xOffset, (int) (1.5 * radius * yc + radius)+yOffset,
-						(int) radius, 6, Math.PI / 2);
+				gamePeace[xc][yc].update( (int)(hrad + yc * hrad + 2* hrad * xc)+xOffset, (int) (1.5 * radius * yc + radius)+yOffset,(int) radius, 6, Math.PI / 2);
 	}
 		
 				BoardTools.setBackground(Global.windowWidth,Global.windowHeight);
