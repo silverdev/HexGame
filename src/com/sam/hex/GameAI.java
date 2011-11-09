4c3f08f5366c90bf3dc41e8065755f8f55f25f80
Silverdev(Sam) <laane2@illinois.edu>
2011-11-08 20:30:52 -0600

    u

--- src/com/sam/hex/GameAI.java
+++ src/com/sam/hex/GameAI.java
@@ -1,5 +1,11 @@
 package com.sam.hex;
 
-public class GameAI {
+public class GameAI implements playingEntity{
+
+	@Override
+	public void getPlayerTurn(byte[] gameBoard) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
