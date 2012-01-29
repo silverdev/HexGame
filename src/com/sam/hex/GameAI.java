c279b59575e4d2d392456ec03033567067880e2f
Nick Jeffrey <njeffre2@illinois.edu>
2012-01-29 15:01:06 -0600

    nothing

--- src/com/sam/hex/GameAI.java
+++ src/com/sam/hex/GameAI.java
@@ -6,12 +6,13 @@ import java.util.List;
 
 
 public class GameAI implements PlayingEntity { 
-	byte team;
+	byte team;// 1 is left-right, 2 is top-down
 	byte difficalty;
 	byte[][] gameBoard;
 	int[] n={0,0},m = {0,0};//n is the leftmost AI move, m is the rightmost AI move
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	ArrayList<List<List<Integer>>> pairs = new ArrayList();//List of pair-pieces
+	
 	
 	public GameAI(byte teamNumberT,byte difficaltyT){
 		team=teamNumberT;
@@ -32,6 +33,9 @@ public class GameAI implements PlayingEntity {
 	}
 
 	public void makeMove(){
+		nickMove();
+	}
+	private void willMove(){
 		/**
 		 * Will's AI
 		 * */
@@ -66,7 +70,7 @@ public class GameAI implements PlayingEntity {
 		}
 		else{
 			//Check if one of our pairs is being attacked, and fill in the alternate if so
-			for(int x=0; x<pairs.size(); x++){
+			for(int x=0; x<pairs.size(); x++){ 
 				if(gameBoard[pairs.get(x).get(0).get(0)][pairs.get(x).get(0).get(1)]!=0){
 					Global.gamePiece[pairs.get(x).get(1).get(0)][pairs.get(x).get(1).get(1)].setTeam(team);
 					pairs.remove(x);
@@ -79,7 +83,7 @@ public class GameAI implements PlayingEntity {
 			
 			//Check if we should extend to the left
 			if((n[0]-2) > 0){
-				if(gameBoard[n[1]-1][n[0]-1]!=0){
+				if(gameBoard[n[1]-1][n[0]-1]==0){
 					Global.gamePiece[n[1]+1][n[0]-2].setTeam(team);
 					ArrayList<List<Integer>> pair = new ArrayList();
 					ArrayList<Integer> cord = new ArrayList();
@@ -91,7 +95,7 @@ public class GameAI implements PlayingEntity {
 					pair.add(cord);
 					pairs.add(pair);
 				}
-				else if(gameBoard[n[1]+1][n[0]-2]!=0){
+				else if(gameBoard[n[1]+1][n[0]-2]==0){
 					Global.gamePiece[n[1]-1][n[0]-1].setTeam(team);
 					ArrayList<List<Integer>> pair = new ArrayList();
 					ArrayList<Integer> cord = new ArrayList();
@@ -106,8 +110,8 @@ public class GameAI implements PlayingEntity {
 			}
 			
 			//Check if we should extend to the right
-			if((m[0]+2) > gameBoard.length-1){
-				if(gameBoard[m[1]-1][m[0]+2]!=0){
+			if((m[0]+2) < gameBoard.length-1){
+				if(gameBoard[m[1]-1][m[0]+2]==0){
 					Global.gamePiece[m[1]+1][m[0]+1].setTeam(team);
 					ArrayList<List<Integer>> pair = new ArrayList();
 					ArrayList<Integer> cord = new ArrayList();
@@ -119,7 +123,7 @@ public class GameAI implements PlayingEntity {
 					pair.add(cord);
 					pairs.add(pair);
 				}
-				else if(gameBoard[m[1]+1][m[0]+1]!=0){
+				else if(gameBoard[m[1]+1][m[0]+1]==0){
 					Global.gamePiece[m[1]-1][m[0]+2].setTeam(team);
 					ArrayList<List<Integer>> pair = new ArrayList();
 					ArrayList<Integer> cord = new ArrayList();
@@ -134,8 +138,60 @@ public class GameAI implements PlayingEntity {
 			}
 		}
 		
-		/*
->>>>>>> 16cd4a96f5bfb3f12819a9302f8097525e26e952
+	}
+	private void nickMove(){
+		int[][] moves=new int[gameBoard.length][gameBoard.length];
+		 
+		 for(int x=0; x<gameBoard.length; x++){
+			for(int y=0; y<gameBoard[x].length; y++){
+				if(gameBoard[x][y]!=0){
+					moves[x][y]=-10000;
+				}
+				else{
+					//Actually calculate value
+				}
+				if(gameBoard[x][y]!=0 && gameBoard[x][y]!=team){
+					
+				}
+				if(gameBoard[x][y]==team){
+					//Look through all pieces and find distance from my piece
+					for(int i=0; i<gameBoard.length; i++)
+						for(int j=0; j<gameBoard[i].length; j++){
+							if(gameBoard[i][j]==0){
+								//TODO: calculate distance
+								if(team==1){
+									
+								}
+								if(team==2){
+									
+								}
+							}
+							
+						}
+				}
+			}
+		}
+		 
+		 int max=moves[0][0];
+		 ArrayList<RegularPolygonGameObject> possible=new ArrayList<RegularPolygonGameObject>();
+		 possible.add(Global.gamePiece[0][0]);
+		 for(int x=0; x<gameBoard.length; x++){
+				for(int y=0; y<gameBoard[x].length; y++){
+					if (moves[x][y]==max){
+						possible.add(Global.gamePiece[x][y]);
+					}
+					else if(moves[x][y]>max){
+						 max=moves[x][y];
+						 possible=new ArrayList<RegularPolygonGameObject>();
+						 possible.add(Global.gamePiece[x][y]);
+					}
+				}
+			}
+		 int move=(int)(possible.size()*Math.random());
+		 possible.get(move).setTeam(team);
+	}
+	
+	private void badMove(){
 		int moves=1;
 		for(int x=0; x<gameBoard.length; x++){
 			for(int y=0; y<gameBoard[x].length; y++){
@@ -143,7 +199,7 @@ public class GameAI implements PlayingEntity {
 			}
 		}
 		moves*=Math.random();
-		for(int x=0; x<gameBoard.length; x++){
+		for(int x=0; x<gameBoard.length; x++)
 			for(int y=0; y<gameBoard[x].length; y++){
 				if(gameBoard[x][y]==0) {
 					moves--;
@@ -153,7 +209,6 @@ public class GameAI implements PlayingEntity {
 					moves=-10;
 				}	
 			}
-		}*/
 	}
 	
 }
